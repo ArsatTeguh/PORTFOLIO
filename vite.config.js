340687240495805c2fb4c98967e73c0ba0276e93
arsyatteguh <arsyatteguh@gmail.com>
2023-07-25 04:54:30 +0700

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    outDir: "dist",
     chunkSizeWarningLimit: 10000,
     rollupOptions: {
       output: {
