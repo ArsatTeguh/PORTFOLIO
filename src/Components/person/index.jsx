111e03ce52c9bfea26ecb36ffa5476c7c1f811b7
arsyatteguh <arsyatteguh@gmail.com>
2023-09-12 22:51:24 +0700

    preforma

--- src/Components/person/index.jsx
+++ src/Components/person/index.jsx
@@ -34,14 +34,14 @@ const Person = () => {
               </div>
               <motion.div
                 className="btn-person"
-                animate={{
-                  y: [0, -20], // Menggeser dari -100 ke 0
-                }}
-                transition={{
-                  duration: 2, // Durasi animasi pergeseran
-                  repeat: Infinity, // Mengulang tak terbatas
-                  repeatType: "reverse", // Mengubah arah pergeseran saat mengulang
-                }}
+                // animate={{
+                //   y: [0, -20], // Menggeser dari -100 ke 0
+                // }}
+                // transition={{
+                //   duration: 2, // Durasi animasi pergeseran
+                //   repeat: Infinity, // Mengulang tak terbatas
+                //   repeatType: "reverse", // Mengubah arah pergeseran saat mengulang
+                // }}
               >
                 <span>Front-end Developer</span>
               </motion.div>
